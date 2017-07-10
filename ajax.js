cbed910df653d81848309b8785ea09efdf86dec4
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-10 16:11:17 -0400

    completed step 3

--- ajax.js
+++ ajax.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 var button = document.getElementById('run_request');
+var pingPong = document.getElementById('ping_pong');
+var sectionOne = document.querySelector('section');
 
   $.ajax({
     url: 'http://first-ajax-api.herokuapp.com/',
@@ -11,6 +13,23 @@ var button = document.getElementById('run_request');
 
   button.addEventListener('click', function() {
     console.log('CLICKED AJAX BUTTON')
+  });
+
+
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/ping',
+    method: 'GET',
+    data: '{}',
+    dataType: 'text'
+  }).done(function (responseData) {
+    console.log(responseData)
+
+    sectionOne.append(responseData)
+
+  });
+
+  pingPong.addEventListener('click', function() {
+    console.log('CLICKED PINGPONG BUTTON')
   });
 
 });
