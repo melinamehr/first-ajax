a7f5b5e27eff37b4d328a8ac16cab55bd95d8223
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-11 15:36:44 -0400

    completed step 9 and fixed errors

--- ajax.js
+++ ajax.js
@@ -9,35 +9,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var button = document.getElementById('run_request');
   button.addEventListener('click', function() {
-    console.log('CLICKED AJAX BUTTON')
+    console.log('CLICKED AJAX BUTTON');
   });
 
   var buttonPing = document.getElementById('ping_pong');
   buttonPing.addEventListener('click', function() {
-  var sec = document.querySelector('#step3456');
+    var sec = document.querySelector('#step3456');
 
-  $.ajax({
-    url: 'http://first-ajax-api.herokuapp.com/ping',
-    method: 'GET',
-    data: '{}',
-    dataType: 'text'
-  }).done(function (responseData) {
-    console.log(responseData);
-    sec.append(responseData);
-  }).fail(function () {
-    var failMessage = "Didn't work but we'll try again!";
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      data: '{}',
+      dataType: 'text'
+    }).always(function() {
+      var alwaysMessage = "Hey the request finished!";
+      console.log(alwaysMessage);
+      sec.append(alwaysMessage);
+    }).done(function (responseData) {
+      console.log(responseData);
+      sec.append(responseData);
+    }).fail(function () {
+      var failMessage = "Didn't work but we'll try again!";
       console.log(failMessage);
       sec.append(failMessage);
     });
-  }).always(function() {
-    var alwaysMessage = "Hey the request finished!"
-    console.log(alwaysMessage);
-    sec.append(alwaysMessage);
-    });
+  });
 
-    var buttonCount = document.getElementById('button_count');
-      buttonCount.addEventListener('click', function() {
-        var secCount = document.getElementById('step7')
+  var buttonCount = document.getElementById('button_count');
+  buttonCount.addEventListener('click', function() {
+    var secCount = document.getElementById('step7');
 
     $.ajax({
       url: 'http://first-ajax-api.herokuapp.com/count',
@@ -48,10 +48,11 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log(responseData);
       secCount.append(responseData);
     });
+  });
 
-    var buttonTime = document.getElementById('ajax_time');
-    var sectionTime = document.querySelector('#step8')
-      buttonTime.addEventListener('click', function() {
+  var buttonTime = document.getElementById('ajax_time');
+  var sectionTime = document.querySelector('#step8');
+  buttonTime.addEventListener('click', function() {
 
     $.ajax({
       url: 'http://first-ajax-api.herokuapp.com/time',
@@ -59,12 +60,26 @@ document.addEventListener("DOMContentLoaded", function() {
       data: {
         timezone: 'Pacific/Honolulu'
       },
-      dataType: 'text',
+      dataType: 'text'
     }).done(function(responseData) {
       console.log(responseData);
       sectionTime.append(responseData);
+    });
+  });
+  var buttonCar = document.getElementById('ajax_car');
+  var secCar = document.querySelector('#step9');
+  buttonCar.addEventListener('click', function() {
 
-    })
-  })
-})
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      method: 'GET',
+      data: '{}',
+      dataType: 'html'
+    }).done(function(responseData) {
+      console.log(responseData);
+      var carsList = document.getElementById('cars');
+      carsList.innerHTML = responsedData;
+      secCar.append(carsList);
+    });
+  });
 });
